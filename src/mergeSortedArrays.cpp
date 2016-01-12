f42ac450ffd2180041e07e62f722557dc20aee57
Bhavishya9 <Bhavishya9@missionrnd.com>
2016-01-13 00:03:11 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -21,6 +21,92 @@ struct transaction {
 	char description[20];
 };
 
+void converter(char *charDate, int intDate[])//function converts char array of date into integer array
+{
+	int j = 0;
+	for (int i = 0; charDate[i] != '\0'; i++)
+	{
+		if (charDate[i] == '-')
+		{
+			j++;
+			continue;
+		}
+		else
+		{
+			intDate[j] = intDate[j] * 10 + (charDate[i] - '0');
+		}
+	}
+}
+
+
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	if (A == NULL || B == NULL || ALen <= 0 || BLen <= 0)
+		return NULL;
+	int transactionDate1[3] = { 0 }, transactionDate2[3] = { 0 };
+	int flag1 = 0, flag2 = 0, index = 0, index1 = 0, index2 = 0;
+	struct transaction *result = (struct transaction*)malloc(6 * sizeof(struct transaction));
+	while (index1<ALen && index2<BLen)
+	{
+		if (flag1 == 0)
+		{
+			converter((A + index1)->date, transactionDate1);
+		}
+		if (flag2 == 0)
+		{
+			converter((B + index2)->date, transactionDate2);
+		}
+		for (int i = 2; i >= 0; i--)
+		{
+			if (transactionDate1[i] > transactionDate2[i])
+			{
+				result[index++] = B[index2];
+				index2++;
+				flag1 = 1;
+				flag2 = 0;
+				for (int j = 0; j < 3; j++)
+					transactionDate2[j] = 0;
+				break;
+			}
+			else if (transactionDate1[i] < transactionDate2[i])
+			{
+				result[index++] = A[index1];
+				index1++;
+				for (int j = 0; j < 3; j++)
+					transactionDate1[j] = 0;
+				flag2 = 1;
+				flag1 = 0;
+				break;
+			}
+			else
+			{
+				if (i == 0)
+				{
+					result[index++] = A[index1];
+					result[index++] = B[index2];
+					index1++;
+					index2++;
+					flag1 = 0;
+					flag2 = 0;
+					for (int j = 0; j < 3; j++)
+					{
+						transactionDate1[j] = 0;
+						transactionDate2[j] = 0;
+					}
+				}
+			}
+		}
+	}
+	while (index1 < ALen && index < (ALen + BLen))
+	{
+		result[index++] = A[index1++];
+	}
+	while (index2 < BLen && index < (ALen + BLen))
+	{
+		result[index++] = B[index2++];
+	}
+	for (int index = 0; index < (ALen + BLen); index++)
+	{
+		printf("%d", result[index].amount);
+	}
+	return result;
 }
--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -22,6 +22,84 @@ struct transaction {
 	char description[20];
 };
 
+void convert_Date(char *charDate, int intDate[])//function converts char array of date into integer array
+{
+	int j = 0;
+	for (int i = 0; charDate[i] != '\0'; i++)
+	{
+		if (charDate[i] == '-')
+		{
+			j++;
+			continue;
+		}
+		else
+		{
+			intDate[j] = intDate[j] * 10 + (charDate[i] - '0');
+		}
+	}
+}
+
 struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	if (A == '\0' || B == '\0' || ALen <= 0 || BLen <= 0)//checks for all the invalid input cases
+		return NULL;
+	int transactionDate1[3] = { 0 }, transactionDate2[3] = { 0 };//take two temporary arrays
+	int flag1 = 0, flag2 = 0;
+	int index = 0, index1 = 0, index2 = 0, length = 0;
+	if (ALen <= BLen)//check which of the two given lengths is minimum
+		length = ALen;
+	else
+		length = BLen;
+	struct transaction *result = (struct transaction*)malloc(length*sizeof(transaction));//with the minimum length create the result struct array
+	int count = 0;//keep a count of common elements
+	while (index1 < ALen && index1 < BLen)//run the loop until one of the array ends
+	{
+		if (flag1 == 0)//if flag1=0 then convert the char date in A array into integer array
+		{
+			convert_Date((A + index1)->date, transactionDate1);
+		}
+		if (flag2 == 0)//if flag2=0 then convert the char date in B array into integer array
+		{
+			convert_Date((B + index2)->date, transactionDate2);
+		}
+		for (int i = 2; i >= 0; i--)//compare the dates in the two temporary integer arrays
+		{
+			if (transactionDate1[i] > transactionDate2[i])//if A is greater then copy B
+			{
+				index2++;// increment the index of B
+				flag1 = 1;
+				flag2 = 0;
+				for (int j = 0; j < 3; j++)//make second temporary integer array to zero
+					transactionDate2[j] = 0;
+				break;
+			}
+			else if (transactionDate1[i] < transactionDate2[i])//if A is less then copy A
+			{
+				index1++;// increment the index of A
+				for (int j = 0; j < 3; j++)// make first temporary integer array to zero
+					transactionDate1[j] = 0;
+				flag2 = 1;
+				flag1 = 0;
+				break;
+			}
+			else
+			{
+				if (i == 0)//if the dates are equal i.e. years and months and day are equal
+				{
+					result[index++] = A[index1++];//copy the value of A[index1] or B[index2] and increment the result, A to next index
+					index2++;//increment the B to next index
+					count++;//count the number of values which are equal
+					flag1 = 0;
+					flag2 = 0;
+					for (int j = 0; j < 3; j++)// make both the temporary integer arrays to zero
+					{
+						transactionDate1[j] = 0;
+						transactionDate2[j] = 0;
+					}
+				}
+			}
+		}
+	}
+	if (count == 0)//if no values are equal then return zero
+		return NULL;
+	return result;//else return the result array
 }
